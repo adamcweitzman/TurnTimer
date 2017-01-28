1335a493fa1f510e885f195cfbcb39d26b3b89eb
Adam Cole Weitzman <adamcweitzman@gmail.com>
2017-01-28 13:09:56 -0600

    add a join game function to the main typescript file.

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Injectable  } from '@angular/core';
 import { NavController, NavParams, AlertController } from 'ionic-angular';
 import { CreateNewGamePage } from '../create-new-game/create-new-game';
 import {AngularFire, FirebaseListObservable} from 'angularfire2';
@@ -9,19 +9,26 @@ import {AngularFire, FirebaseListObservable} from 'angularfire2';
   templateUrl: 'hello-ionic.html'
 })
 
+@Injectable()
 export class HelloIonicPage {
   public gameName: any;
   public number_of_players: any;
-  public game: FirebaseListObservable<any>
+  public game: FirebaseListObservable<any>;
   public randomId: any;
   public admin: string;
   public your_name: string;
+  private db: any;
+  private ref: any;
+  private gameRef: any;
+  private joinGame: FirebaseListObservable<any>;
 
 
 
 	constructor(public navCtrl: NavController, public alertCtrl: AlertController, af: AngularFire) {
 		this.game = af.database.list('/games');
-		console.log(this.randomId);
+    // this.db = firebase.database().ref('/');
+    // this.gameRef = firebase.database().ref ('games');
+    // this.gameRef.on('join_game_alert');
 	}
 
    newGameAlert(): void {
@@ -70,6 +77,53 @@ export class HelloIonicPage {
   	});
 
   	prompt.present();
+  } 
+
+  joinGameAlert(af): void {
+     let prompt = this.alertCtrl.create({
+      title: 'Join Game',
+      message: 'Enter game details below',
+      inputs: 
+      [
+        {
+          name: 'firstName',
+          placeholder: 'first name'
+        },
+        {
+          name: 'initials',
+          placeholder: 'initials'
+        },
+        {
+          name: 'gameId',
+          placeholder: 'game Id'
+        },
+      ],
+      buttons: 
+      [
+        {
+          text: 'Save',
+          handler: data => 
+          {
+            this.game.push({
+              first_name: data.firstName,
+              initials: data.initials,
+              gameId: data.gameId
+            })
+          } 
+        }
+      ]
+    });
+
+    prompt.present();
+
+   // let game = af.database.list('games', {
+   //    query: {
+   //        equalTo: 'Kb_yFoF9rw5V5YLTKw6'
+   //    }
+   //  })
+
+    // console.log(game)
   }
+
 
 }
